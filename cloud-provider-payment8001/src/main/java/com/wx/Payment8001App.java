08c83b4b6d57221486628cd893077067aeace903
weishengwx <2234346829@qq.com>
2022-06-20 22:44:56 +0800

    build(新增): 初始化cloud-payment-8001这个子模块

---
+++ cloud-provider-payment8001/src/main/java/com/wx/Payment8001App.java
@@ -0,0 +1,19 @@
+package com.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @author 王兴
+ * @date 2022/6/20 22:36
+ * @message
+ */
+@SpringBootApplication
+public class Payment8001App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Payment8001App.class,args);
+	}
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/wx/entity/Pay.java
@@ -0,0 +1,10 @@
+package com.wx.entity;
+
+/**
+ * @version 1.0
+ * @author 王兴
+ * @date 2022/6/20 22:41
+ * @message
+ */
+public class Pay {
+}
